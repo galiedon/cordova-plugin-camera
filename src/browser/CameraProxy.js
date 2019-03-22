1dce6bb970d8250615da8d3bb7a49833fb4cfd5c
SenchGaliedon <sench_galiedon@outlook.com>
2019-03-22 16:42:38 +0800

    添加了ios端的拍照操作

--- src/browser/CameraProxy.js
+++ src/browser/CameraProxy.js
@@ -113,8 +113,10 @@ function capture (success, errorCallback, opts) {
 
     if (navigator.getUserMedia) {
         navigator.getUserMedia({video: true, audio: false}, successCallback, errorCallback);
-    } else {
-        alert('Browser does not support camera :(');
+    } else if(navigator.mediaDevices.getUserMedia){
+        navigator.mediaDevices.getUserMedia({video: true, audio: false}, successCallback, errorCallback);
+    }else{
+        console.log('Browser does not support camera :(');
     }
 }
 
